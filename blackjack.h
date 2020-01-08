5bee50c29fd0ebffaf6a7e1d7427b04b04784c95
VictoriaSzy <vszymanowski00@stuy.edu>
2020-01-07 21:47:20 -0500

    Added structs - let's use typedef structs!

--- blackjack.h
+++ blackjack.h
@@ -1,1 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
 
+// typedef structs mean that we won't have to type struct countless times in the return types of the functions
+typedef struct {
+  int suit ;
+  int value ;
+} card ;
+
+typedef struct Deck {
+  card * c ;
+  struct Deck * next ;
+} deck ;
+
+typedef struct {
+  deck * hand ;
+  int min ;
+  int max ;
+} player ;
