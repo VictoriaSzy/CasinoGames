7b77667eaecd79d60772e7ce1f6f1031cf4db605
VictoriaSzy <vszymanowski00@stuy.edu>
2020-01-07 21:53:40 -0500

    Created a deck and the 2 players

--- blackjack.c
+++ blackjack.c
@@ -1,5 +1,14 @@
 #include "blackjack.h"
 
+
+
 int main() {
+  deck * d ; // this will be our deck
+  // here are our 2 players for the moment
+  player p ;
+  player dealer ;
+  char response [100] ; // this will be used to hold the player's response
+
+  
   return 0 ;
 }
