aaba904625772f5b54112f7a0ffbd8ae0df64200
Theod0reWu <twu00@stuy.edu>
2020-01-14 22:26:07 -0500

    slots

--- slots.c
+++ slots.c
@@ -1,1 +1,15 @@
 
+/* functionality
+3 slots
+10 symbols per reel
+! @ # $ % ^ & * 7 ?
+players spin three to see if they get any combinations
+
+777 | x100
+$$$ | x10
+@@@ | x3
+### | x2
+?17 | x0
+
+*/
+
--- slots.h
+++ slots.h
@@ -1,1 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+#include <string.h>
+#include <errno.h>
+
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <dirent.h>
+
+#include <math.h>
+
+#include <signal.h>
+
+#define SDL_MAIN_HANDLED
+#include <SDL2/SDL.h>
+
+#define reel 
+
+char * spin(slot * s);
+int is_win(char * sym);
