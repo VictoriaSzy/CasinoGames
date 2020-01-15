d4a5a14ceee850659e222de875b758b798907d85
VictoriaSzy <vszymanowski00@stuy.edu>
2020-01-14 21:52:30 -0500

    Added function headers

--- blackjack.h
+++ blackjack.h
@@ -7,6 +7,7 @@
 typedef struct {
   int suit ; // either a heart (0), club (1), diamond (2), or spade (3)
   int value ; // A will be worth either 1 or 11 and the face cards (J, Q, K) will be worth 10
+  int valid ; // whether we can use it or not: 0 represents ok, 1 is no bueno
 } card ;
 
 typedef struct {
@@ -21,8 +22,7 @@ typedef struct {
 } player ;
 
 // Here are the functions:
-void printCard(card * c) ;
-void printDeck(deck * d) ;
+void printCard(card c) ;
+void printDeck(deck d) ;
 int find_sum(card hand [50]) ;
-void shuffle(deck * d) ;
-void hit(player * p) ;
+void shuffle(deck d) ;
