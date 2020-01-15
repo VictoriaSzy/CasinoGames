04cb5687321aed7e061da9339f83a39e5c637056
VictoriaSzy <vszymanowski00@stuy.edu>
2020-01-14 21:34:38 -0500

    Edited amount of cards held by player and added function headers

--- blackjack.h
+++ blackjack.h
@@ -5,7 +5,7 @@
 
 // this is the struct for a single card
 typedef struct {
-  int suit ; // either a heart (1), club (2), diamond (3), or spade (4)
+  int suit ; // either a heart (0), club (1), diamond (2), or spade (3)
   int value ; // A will be worth either 1 or 11 and the face cards (J, Q, K) will be worth 10
 } card ;
 
@@ -16,10 +16,13 @@ typedef struct {
 
 // this is the struct for the player/dealer - we'll keep track of their hand like this
 typedef struct {
-  card hand [52] ; // we'll know the exact cards the player has
+  card hand [50] ; // we'll know the exact cards the player has
   int sum ; // the sum of the player's cards
 } player ;
 
 // Here are the functions:
 void printCard(card * c) ;
 void printDeck(deck * d) ;
+int find_sum(card hand [50]) ;
+void shuffle(deck * d) ;
+void hit(player * p) ;
