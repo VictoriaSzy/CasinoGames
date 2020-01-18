c2a3de694fbcfb497841fb98db9a38937b9368e2
Theod0reWu <twu00@stuy.edu>
2020-01-17 23:42:46 -0500

    started roulette

--- roulette.c
+++ roulette.c
@@ -1,1 +1,35 @@
-
+/*
+###NNNNN
+##NN   NN
+#NN     NN
+NN       NN
+N         N
+N         N
+NN       NN
+#NN     NN
+##NN   NN
+###NNNNN
+*/
+//1-37,38,39
+/* shoot it has to be able to turn, or maybe not
+   01234
+ 15     11
+14       16
+13       17
+ 12     10
+   85679
+*/
+/*
+       NNNNNNNN
+    NNNN      NNNN    
+  NNN            NNN
+ NN                NN
+NN                  NN
+N                    N
+N                    N
+NN                  NN 
+ NN                NN
+  NNN            NNN 
+    NNNN      NNNN
+       NNNNNNNN
+*/
--- roulette.h
+++ roulette.h
@@ -1,1 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+#include <string.h>
+#include <errno.h>
+
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <dirent.h>
+
+#include <math.h>
+#include <signal.h>
+
+//#define SDL_MAIN_HANDLED
+//#include <SDL2/SDL.h>
+
+#define WHEEL {'!', '@', '#', '$', '%', '^', '&', '*', '7', '?'}
+#include "slots.h"
+
+char * spin() ;
+int is_win() ;
+int roulette_game(int money);
+//int super_sleep(int milliseconds);
