bbe7841a75b8d9fd075f6976fe9902a48cb860cf
Theod0reWu <twu00@stuy.edu>
2020-01-17 23:59:40 -0500

    more designs

--- roulette.c
+++ roulette.c
@@ -1,15 +1,3 @@
-/*
-###NNNNN
-##NN   NN
-#NN     NN
-NN       NN
-N         N
-N         N
-NN       NN
-#NN     NN
-##NN   NN
-###NNNNN
-*/
 //1-37,38,39
 /* shoot it has to be able to turn, or maybe not
    01234
@@ -18,6 +6,20 @@ NN       NN
 13       17
  12     10
    85679
+*/
+/*       V
+       30010203
+    2829      0405    
+  N27            06N
+ 26                07
+25                  08
+4                    0
+2                    9
+23                  10 
+ 22                11
+  N21            12N 
+    2019      1314
+       18171615
 */
 /*
        NNNNNNNN
@@ -32,4 +34,34 @@ NN                  NN
   NNN            NNN 
     NNNN      NNNN
        NNNNNNNN
-*/
+  */
+/*
+       01N02N03
+    NN00      04NN    
+  N23            05N
+ 22                06
+21                  07
+0                    0
+2                    8
+19                  09 
+ 18                10
+  N17            11N 
+    NN16      12NN
+       15N14N13
+  */
+/*
+        NNNNNNNN
+     NNNN      NNNN    
+   NNN            NNN
+  NN                NN
+ NN                  NN
+NN                    NN
+N                      N
+N                      N
+NN                    NN
+ NN                  NN 
+  NN                NN
+   NNN            NNN 
+     NNNN      NNNN
+        NNNNNNNN
+  */
